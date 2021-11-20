91d82699a2097c076404627c354929c1c725c5b9
zairosorio <sairosorio@hotmail.com>
2021-11-20 16:17:07 -0500

    se creo actor y se agrega driver

--- src/test/java/co/com/qvision/certificacion/demoqa/stepdefinitions/AutomationStepDefinition.java
+++ src/test/java/co/com/qvision/certificacion/demoqa/stepdefinitions/AutomationStepDefinition.java
@@ -1,14 +1,32 @@
 package co.com.qvision.certificacion.demoqa.stepdefinitions;
 
+import io.cucumber.java.Before;
 import io.cucumber.java.es.Cuando;
 import io.cucumber.java.es.Dado;
 import io.cucumber.java.es.Entonces;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.actions.Open;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+import net.thucydides.core.annotations.Managed;
+import org.openqa.selenium.WebDriver;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 
 public class AutomationStepDefinition {
 
 
+    @Managed
+    WebDriver driver;
+
+    @Before
+    public void sepUp(){
+        OnStage.setTheStage(new OnlineCast());
+        OnStage.theActorCalled("name_actor").can(BrowseTheWeb.with(driver));
+    }
+
     @Dado("que el usuario ingresa a la pagina Demo")
     public void queElUsuarioIngresaALaPaginaDemo() {
+        theActorInTheSpotlight().attemptsTo(Open.url(""));
 
     }
     @Cuando("Llenar el formulario vacio")
